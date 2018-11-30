8ba5af973c6949d2b5bc3a42a06acfa33e6def90
amishmishra <amish_mishra@taylor.edu>
2018-11-30 17:34:07 -0500

    create .js file and .gitignore

---
+++ hapi-server.js
@@ -0,0 +1,61 @@
+// Standard Node modules
+const Path = require("path");
+
+// Knex
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: "faraday.cse.taylor.edu",
+        database: "amish_mishra",
+        user:"amish_mishra",
+        password: "bonepoje"
+    }
+});
+
+// Hapi
+const Joi = require("joi"); // Input validation
+const Hapi = require("hapi"); // Server
+
+const server = Hapi.server({
+    host: "localhost",
+    port: 3000,
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, "dist")
+        }
+    }
+});
+
+async function init() {
+    // Show routes at startup.
+    await server.register(require("blipp"));
+
+    // Output logging information.
+    await server.register({
+        plugin: require("hapi-pino"),
+        options: {
+            prettyPrint: true
+        }
+    });
+
+    // Configure static file service.
+    await server.register([require("inert"), require("lout")]);
+
+    // Configure routes.
+    server.route([
+        {
+        }
+    ]);
+
+    // Start the server.
+    await server.start();
+    server.logger().info(`Server running at ${server.info.uri}`);
+}
+
+process.on("unhandledRejection", err => {
+    server.logger().error(err);
+    process.exit(1);
+});
+
+// Go!
+init();
